3aa978d5f621ce0a0f00022c9793bb3e680aefe2
Miguel Latuf <latufmiguel@gmail.com>
2018-07-27 20:13:42 -0300

    Instalacion Clarity y modulo de ruteo

--- freedom-system/src/app/app.module.ts
+++ freedom-system/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './common/menu/menu.component';
+import { RoutingModule } from './/routing.module';
+import { ClarityModule } from '@clr/angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RoutingModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
