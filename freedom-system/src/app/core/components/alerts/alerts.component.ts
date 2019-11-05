d692829698084817d7f0b0a76542b547e5bb9697
Miguel Latuf <latufmiguel@gmail.com>
2019-11-05 08:19:08 -0300

    Unify messages in a single component

--- freedom-system/src/app/core/components/alerts/alerts.component.ts
+++ freedom-system/src/app/core/components/alerts/alerts.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Alert, AlertType } from '../../classes/alert';
 import { AlertService } from '../../services/alert/alert.service';
+import { MatSnackBar, MatDialog } from '@angular/material';
+import { ModalComponent } from '../modal/modal.component';
+import { ErrorService } from '../../services/alert/error.service';
 
 @Component({
   selector: 'alerts',
@@ -11,7 +14,10 @@ export class AlertsComponent implements OnInit {
 
   alerts: Alert[] = [];
  
-  constructor(private alertService: AlertService) { }
+  constructor(private alertService: AlertService, 
+    public dialog: MatDialog, 
+    private _snackbar: MatSnackBar, 
+    private errorService: ErrorService) { }
 
   ngOnInit() {
     this.alertService.getAlert().subscribe((alert: Alert) => {
@@ -22,6 +28,24 @@ export class AlertsComponent implements OnInit {
       }
       // add alert to array
       this.alerts.push(alert);
+      if (alert.type === AlertType.Error) {
+        const dialogRef = this.dialog.open(ModalComponent, {
+          disableClose: true,
+          data: {
+            title: 'Ops! Al parecer tenemos problemas', 
+            text: this.errorService.getErrorText(alert.message),
+            hasError: true
+          }
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          if(result) { return }
+        });
+      } else {
+        this._snackbar.open(alert.message,"OK", {
+          duration: 2000,
+          panelClass: 'snackbar-container'
+        });
+      }
     });
   }
 
