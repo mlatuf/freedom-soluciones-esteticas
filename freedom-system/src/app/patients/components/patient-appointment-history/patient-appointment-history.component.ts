76a6451108870be6242d131047869267f57bca18
Miguel Latuf <latufmiguel@gmail.com>
2020-02-12 08:12:23 -0300

    appointment history modal

--- freedom-system/src/app/patients/components/patient-appointment-history/patient-appointment-history.component.ts
+++ freedom-system/src/app/patients/components/patient-appointment-history/patient-appointment-history.component.ts
@@ -1,31 +1,49 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 import { AppointmentPatient } from '../../../patients/classes/appointment-patient';
 import { PatientService } from '../../../patients/services/patient.service';
 import { AlertService } from '../../../core/services/alert/alert.service'
+import { ApplicationStateService } from '../../../core/services/aplication-state/aplication-state.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'patient-appointment-history',
   templateUrl: './patient-appointment-history.component.html',
-  styleUrls: ['./patient-appointment-history.component.scss']
+  styleUrls: ['./patient-appointment-history.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class PatientAppointmentHistoryComponent implements OnChanges {
 
   @Input() patientId: string;
-  appointmentHistory: AppointmentPatient[];
+  mobileView: Boolean = false;
+  displayedColumns: string[] = ['date', 'areas', 'status', 'observations'];
+  displayedMobileColumns: string[] = ['expand', 'date', 'areas'];
+  dataSource: MatTableDataSource<AppointmentPatient>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  constructor(private patientService: PatientService, private spinner: NgxSpinnerService, private alertService: AlertService ) { 
-    this.appointmentHistory = [];
-  }
+  constructor(private patientService: PatientService, 
+    private spinner: NgxSpinnerService, 
+    private alertService: AlertService,
+    private aplicationStateService: ApplicationStateService ) { }
 
   ngOnChanges() {
-    this.appointmentHistory = [];
+    this.mobileView = this.aplicationStateService.getIsMobileResolution();
     if (this.patientId) {
       this.spinner.show();
       this.patientService.getPatientHistory$(this.patientId).subscribe(
         response => {
-          this.appointmentHistory = response;
+          this.dataSource = new MatTableDataSource(response);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
           this.spinner.hide();
         },
         error => {
