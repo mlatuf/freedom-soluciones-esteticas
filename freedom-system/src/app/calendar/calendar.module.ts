65a46e2d04d6cd1f6fc05dfd18c2eae582847ffe
Miguel Latuf <latufmiguel@gmail.com>
2020-02-19 07:56:19 -0300

    calendar history material

--- freedom-system/src/app/calendar/calendar.module.ts
+++ freedom-system/src/app/calendar/calendar.module.ts
@@ -3,11 +3,25 @@ import { ClarityModule } from '@clr/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
-import { MatButtonModule, MatIconModule, MatCardModule, MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { 
+  MatButtonModule, 
+  MatIconModule, 
+  MatCardModule, 
+  MatDatepickerModule,
+  MatFormFieldModule, 
+  MatNativeDateModule, 
+  MatInputModule, 
+  MatTableModule, 
+  MatPaginatorModule, 
+  MatSortModule, 
+  MatDividerModule, 
+  MatDialogModule } from '@angular/material';
 
 import { CoreModule } from '../core/core.module';
 
 import { CalendarComponent } from './components/calendar.component';
+import { CalendarHistoryComponent } from './components/calendar-history/calendar-history.component';
+
 import { MonthNamePipe } from './pipes/month-name.pipe';
 import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
 
@@ -27,12 +41,16 @@ import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule, 
+    MatDividerModule,
+    MatDialogModule
   ],
   declarations: [
     CalendarComponent,
-    MonthNamePipe
+    MonthNamePipe,
+    CalendarHistoryComponent
   ],
+  entryComponents: [CalendarHistoryComponent],
   providers: [AngularFireAuthGuard]
 })
 export class CalendarModule { }
--- freedom-system/src/app/calendar/components/calendar.component.ts
+++ freedom-system/src/app/calendar/components/calendar.component.ts
@@ -1,31 +1,39 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatPaginator, MatSort, MatTableDataSource, MatDialog } from '@angular/material';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
-import { AlertService } from '../../core/services/alert/alert.service'
-import { CalendarService } from '../services/calendar.service'
+import { AlertService } from 'src/app/core/services/alert/alert.service'
+import { CalendarService } from 'src/app/calendar/services/calendar.service'
+import { ApplicationStateService } from 'src/app/core/services/aplication-state/aplication-state.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 import { Day } from '../classes/day';
-import { ApplicationStateService } from '../../core/services/aplication-state/aplication-state.service';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatPaginator, MatSort, MatTableDataSource, MatDialog } from '@angular/material';
+import { CalendarHistoryComponent } from 'src/app/calendar/components/calendar-history/calendar-history.component'
 
 @Component({
   selector: 'calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+  styleUrls: ['./calendar.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class CalendarComponent implements OnInit {
   
   mobileView: Boolean;
   showNewDateForm: Boolean;
-  openHistory: Boolean;
   newDate: Day;
   calendar: Day[];
-  calendarHistory: Day[];
   dateForm: FormGroup;
 
   displayedColumns: string[] = ['year', 'month', 'days'];
+  displayedMobileColumns: string[] = ['expand', 'year', 'month'];
   dataSource: MatTableDataSource<Day>;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
@@ -34,12 +42,13 @@ export class CalendarComponent implements OnInit {
     private calendarService: CalendarService, 
     private spinner: NgxSpinnerService, 
     private alertService: AlertService,
-    private fb: FormBuilder) { 
-    this.showNewDateForm = this.openHistory = false;
+    private fb: FormBuilder,
+    public dialog: MatDialog) { 
+    this.showNewDateForm  = false;
   }
   
   ngOnInit() {
-    this.calendar = this.calendarHistory = [];
+    this.calendar = [];
     this.mobileView = this.aplicationState.getIsMobileResolution();
     this.newDate = new Day;
     this.newDate.isFinished = false;
@@ -49,19 +58,14 @@ export class CalendarComponent implements OnInit {
     this.getCalendar(); 
   }
 
-  showCalendarHistory(): void {
-    this.openHistory = !this.openHistory;
-    this.spinner.show();
-    this.calendarService.getCalendarHistory$().subscribe(
-      response => {
-        this.calendarHistory = response;
-        this.spinner.hide();
-      },
-      error => {
-        this.spinner.hide();
-        this.alertService.error(error);
+  showCalendarHistory() {
+    const dialogRef = this.dialog.open(CalendarHistoryComponent, {
+      width: '80%',
+      data: {
+        title: 'Historial de dias',
       }
-    );
+    });
+    dialogRef.afterClosed().subscribe(result => {});
   }
 
   private getCalendar() {
